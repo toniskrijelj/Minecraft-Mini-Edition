77304933b714ff51ddef4f72749f8fe1516f4ced
NoHaxJustSoFlo <58442140+NoHaxJustSoFlo@users.noreply.github.com>
2020-05-21 12:01:17 +0200

    Sandbox

--- Assets/Scripts/Testing.cs
+++ Assets/Scripts/Testing.cs
@@ -7,7 +7,7 @@ public class Testing : MonoBehaviour
 	public void Awake()
 	{
 		new BlockGrid(16, 9, 1, new Vector3(-9, -5));
-		for (int i = 0; i < 10; i++)
+		for (int i = 0; i < 16; i++)
 		{
 			SpriteRenderer spriteRenderer = Object.Instantiate(BlockData.blockData.blockPrefab, BlockGrid.Instance.GetWorldPosition(i, 0), Quaternion.identity);
 			BlockGrid.Instance.SetBlock(i, 0, new Block(1, ToolType.None, ToolMaterial.All, spriteRenderer, "BLOCK NIGGA " + i, BlockData.blockData.oakPlanksTexture));

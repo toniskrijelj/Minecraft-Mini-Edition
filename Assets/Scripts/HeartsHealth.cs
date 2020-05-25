81f63f0a0bd27bd0c19b74f6dc7879d5d7d81ea5
DoktorSmrda <62447233+DoktorSmrda@users.noreply.github.com>
2020-05-25 17:03:13 +0200

    HealthSystem

---
+++ Assets/Scripts/HeartsHealth.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class HeartsHealth : MonoBehaviour
+{
+    [SerializeField] private Sprite heartSpriteFull;
+    [SerializeField] private Sprite heartSpriteHalf;
+    [SerializeField] private Sprite heartSpriteEmpty;
+
+    private List<HeartImage> heartImageList;
+    private HeartsHealthSystem heartsHealthSystem;
+
+    private void Awake()
+    {
+        heartImageList = new List<HeartImage>();
+    }
+
+    private void Start()
+    {
+        HeartsHealthSystem heartsHealthSystem = new HeartsHealthSystem(4);
+        SetHeartsHealthSystem(heartsHealthSystem);
+
+        heartsHealthSystem.Damage(3);
+    }
+
+    public void SetHeartsHealthSystem(HeartsHealthSystem heartsHealthSystem)
+    {
+        this.heartsHealthSystem = heartsHealthSystem;
+
+        List<HeartsHealthSystem.Heart> heartList = heartsHealthSystem.GetHeartList();
+        Vector2 heartAnchoredPosition = new Vector2(0, 0);
+        for(int i = 0; i < heartList.Count; i++)
+        {
+            HeartsHealthSystem.Heart heart = heartList[i];
+            CreateHeartImage(heartAnchoredPosition).SetHeartFragments(heart.GetFragmentAmount());
+            heartAnchoredPosition += new Vector2(30, 0);
+        }
+
+        heartsHealthSystem.OnDamaged += HeartsHealthSystem_OnDamaged;
+    }
+
+    private void HeartsHealthSystem_OnDamaged(object sender, System.EventArgs e)
+    {
+        List<HeartsHealthSystem.Heart> heartList = heartsHealthSystem.GetHeartList();
+        for(int i = 0; i < heartImageList.Count; i++)
+        {
+            HeartImage heartImage = heartImageList[i];
+            HeartsHealthSystem.Heart heart = heartList[i];
+            heartImageList[i].SetHeartFragments(heartsHealthSystem.GetHeartList()[i].GetFragmentAmount());
+        }
+    }
+
+    private HeartImage CreateHeartImage(Vector2 anchoredPosition)
+    {
+        GameObject heartGameObject = new GameObject("HeartFull", typeof(Image));
+        heartGameObject.transform.parent = transform;
+
+        heartGameObject.GetComponent<Image>().sprite = heartSpriteFull;
+        heartGameObject.transform.localPosition = Vector3.zero;
+
+        heartGameObject.GetComponent<RectTransform>().anchoredPosition = anchoredPosition;
+        heartGameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(10, 10);
+
+        Image heartImageUI = heartGameObject.GetComponent<Image>();
+        heartImageUI.sprite = heartSpriteFull;
+
+        HeartImage heartImage = new HeartImage(this, heartImageUI);
+        heartImageList.Add(heartImage);
+
+        return heartImage;
+    }
+
+    public class HeartImage
+    {
+        private Image heartImage;
+        private HeartsHealth heartsHealth;
+        public HeartImage(HeartsHealth heartsHealth, Image heartImage)
+        {
+            this.heartImage = heartImage;
+            this.heartsHealth = heartsHealth;
+        }
+
+        public void SetHeartFragments(int fragments)
+        {
+            switch(fragments)
+            {
+                case 0: heartImage.sprite = heartsHealth.heartSpriteEmpty; 
+                        break;
+                case 1: heartImage.sprite = heartsHealth.heartSpriteHalf;
+                        break;
+                case 2: heartImage.sprite = heartsHealth.heartSpriteFull;
+                        break;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/HeartsHealthSystem.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartsHealthSystem
+{
+    public event EventHandler OnDamaged;
+    private List<Heart> heartList;
+    public HeartsHealthSystem(int heartAmount)
+    {
+        heartList = new List<Heart>();
+        for(int i = 0; i < heartAmount; i++)
+        {
+            Heart heart = new Heart(2);
+            heartList.Add(heart);
+        }
+
+        heartList[heartList.Count - 1].SetFragments(0);
+        heartList[heartList.Count - 2].SetFragments(1);
+    }
+
+    public void Damage(int damageAmount)
+    {
+        for(int i = heartList.Count-1; i>=0; i--)
+        {
+            Heart heart = heartList[i];
+            if(damageAmount > heart.GetFragmentAmount())
+            {
+                damageAmount -= heart.GetFragmentAmount();
+                heart.Damage(heart.GetFragmentAmount());
+            }
+            else
+            {
+                heart.Damage(damageAmount);
+                break;
+            }
+        }
+
+        if(OnDamaged!= null)
+        {
+            OnDamaged(this, EventArgs.Empty);
+        }
+    }
+
+    public List<Heart> GetHeartList()
+    {
+        return heartList;
+    }
+    public class Heart
+    {
+        private int fragments;
+        public Heart(int fragments)
+        {
+            this.fragments = fragments;
+        }
+        public int GetFragmentAmount()
+        {
+            return fragments;
+        }
+
+        public void SetFragments(int fragments)
+        {
+            this.fragments = fragments;
+        }
+        
+        public void Damage(int damageAmount)
+        {
+            if(damageAmount >= fragments)
+            {
+                fragments = 0;
+            }
+            else
+            {
+                fragments -= damageAmount;
+            }
+        }
+    }
+}

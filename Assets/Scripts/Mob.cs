a95651c57a9fe0e7f68b2ddbee33b94a5f155f6d
Meda <medadanilo@gmail.com>
2020-05-25 23:22:49 +0200

    mob prazan

---
+++ Assets/Scripts/Mob.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Mob : MonoBehaviour
+{
+    HeartsHealthSystem hp;
+    protected void Awake()
+    {
+        hp = new HeartsHealthSystem(HeartsAmount());
+    }
+
+    protected abstract int HeartsAmount();
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}

511c2313c7ce456049ff583fc3022835c470c764
Meda <medadanilo@gmail.com>
2020-05-27 18:49:48 +0200

    day night spritovi

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNightCycle : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+}
